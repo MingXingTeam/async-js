1c32a3def101e45a37403376c7803058ff77d3fe
Xing <813106093@qq.com>
2016-03-12 15:43:02 +0800

    add json2

--- what-are-promise.js
+++ what-are-promise.js
@@ -1,19 +1,19 @@
-var getData = $.ajax({
-	type: "GET",
-	url: "data.json"
-});
+// var getData = $.ajax({
+// 	type: "GET",
+// 	url: "data.json"
+// });
 
-getData.then(function(data) {
-	//成功回调
-	$("#test1").html(JSON.stringify(data));
-}, function(error) {
-	$("#test1").html(JSON.stringify(error));
-});
+// getData.then(function(data) {
+// 	//成功回调
+// 	$("#test1").html(JSON.stringify(data));
+// }, function(error) {
+// 	$("#test1").html(JSON.stringify(error));
+// });
 
 var getData = $.get('data.json');
 var getData2 = $.get('data2.json');
 $.when(getData, getData2).then(function(data,data2) {
-
+	$("#test1").html(JSON.stringify(data));
 }, function(){
 	//handle error
 })
